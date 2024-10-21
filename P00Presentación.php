29bc619e48f54f8b5b83d7a33611c50f123d7c09
ZZZTALLION <teresadelcarmen460@gmail.com>
2024-10-21 10:29:30 -0500

    Add files via upload

---
+++ P00Presentación.php
@@ -0,0 +1,22 @@
+<?php
+/* CBTIS 89
+P00Presentación.
+Programa que contiene mis datos personales.
+Teresa del Carmen Sánchez Zaldivar.
+3ªA Programación Matutino.
+*/
+
+echo "DATOS PERSONALES";
+echo "<br>";
+echo "<br>";
+echo "Mi nombre es Teresa del Carmen Sánchez Zaldivar.";
+echo "<br>";
+echo "Tengo 16 años.";
+echo "<br>";
+echo "Estudio en el CBTis89.";
+echo "<br>";
+echo "Soy de tercer semestre de la carrera de Programación A Matutino.";
+echo "<br>";
+echo "Vivo en la ciudad de Durango,Dgo."
+
+?>
